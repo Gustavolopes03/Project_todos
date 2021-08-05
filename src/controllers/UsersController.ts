123bc88bae5e692da7d90655aad594e5edf0919f
Gustavo616 <gustavolavrasmg@hotmail.com>
2021-08-05 13:05:09 -0300

    commit6

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,4 +1,5 @@
-import { request, Request,Response } from "express";
+import { Address } from "cluster";
+import { request, Request, response, Response } from "express";
 import { stringify, v4 as uuid } from "uuid";
 
 interface iuser {
@@ -9,47 +10,47 @@ interface iuser {
     //password: string,
     cpf: number,
     address?: {
-       street:string,
-       number:number,
-       district:string,
-       city:string,
-       state:string,
+        street: string,
+        number: number,
+        district: string,
+        city: string,
+        state: string,
     }
-    todos?:{
-       id:string,
-       title:string,
-       deadline:string,
-       done:boolean,
-       created_at:Date,
+    todos?: {
+        id: string,
+        title: string,
+        deadline: string,
+        done: boolean,
+        created_at: Date,
     }
 
 }
 const users: iuser[] = [];
 
-export default class UsersController{
-    //Criar Usuário \/ 
-    public async create(request: Request, response: Response){
-        
-        const { name,email,birthDate,cpf } = request.body;
+export default class UsersController {
+//Criar Usuário \/ 
+    public async create(request: Request, response: Response) {
 
-        // const userFindall = users.find((user:any)=> {
-        //     return user.name === name,
-        //     user.email === email,
-        //     user.birthDate === birthDate,
-        //     user.cpf === cpf
-        // });
+        const { name, email, birthDate, cpf } = request.body;
 
-        const fuser = users.find((user:any)=>{
+        const userFindall = users.find((user: any) => {
+            return user.name === name,
+                user.email === email,
+                user.birthDate === birthDate,
+                user.cpf === cpf
+        });
+
+        const fuser = users.find((user: any) => {
             return user.cpf === cpf
         });
 
-        if (fuser){
+        if (fuser) {
             return response.status(200).json(fuser)
-        } 
-            
+        }
 
-        const user:iuser =  {
-            id:uuid(),
+
+        const user: iuser = {
+            id: uuid(),
             name,
             email,
             birthDate: birthDate,
@@ -58,94 +59,123 @@ export default class UsersController{
         users.push(user);
         response.status(201).json(users)
     }
-    //Procurar Cpf especifico de Usuário \/
-    public async search(request:Request ,response: Response){
+//Procurar Cpf especifico de Usuário \/
+    public async search(request: Request, response: Response) {
 
-        const {cpf} = request.headers;
+        const { cpf } = request.headers;
 
-        if (cpf){
-        const fuser = users.find((user:any)=>{
-            return user.cpf == cpf
-        });        
+        if (cpf) {
+            const fuser = users.find((user: any) => {
+                return user.cpf == cpf
+            });
             return response.status(200).json(fuser)
         }
         return response.status(200).json(users);
 
     }
-    //Procurar Usuários Maiores de 18 \/
-    public async agefind(request:Request ,response:Response){
+//Procurar Usuários Maiores de 18 \/
+    public async agefind(request: Request, response: Response) {
         const mage: any[] = []
+        const Userage: any[] = []
 
         var data = new Date();
-        var dia = String(data.getDate()).padStart(2, '0');
-        var mes = String(data.getMonth() + 1).padStart(2, '0');
-        var ano = data.getFullYear();
-        var dataAtual = dia + '/' + mes + '/' + ano;
+        var diaAtual = String(data.getDate()).padStart(2, '0');
+        var mesAtual = String(data.getMonth() + 1).padStart(2, '0');
+        var anoAtual = data.getFullYear();
+        var dataAtual = diaAtual + '/' + mesAtual + '/' + anoAtual;
 
         users.forEach(user => {
             const fullDate = String(user.birthDate);
-            var fsplit =fullDate.split('/');
+            var fsplit = fullDate.split('/');
 
-            var uday = fsplit.slice(0,1)
-            var ncdu = Number(uday)
-            var ncdc = Number(dia)
-            //var rd= ncdc - ncdu
+            var userday = fsplit.slice(0, 1)
+            var nUday = Number(userday)
+            var nAday = Number(diaAtual)
 
-            var umonth = fsplit.slice(1,2)
-            var ncmu = Number(umonth)
-            var ncmc = Number(mes)
-            //var rm= ncmc - ncmu
-            
-            var uyear = fsplit.slice(2,3)
-            var ncyu = Number(uyear)
-            var ncyc = Number(ano)
-            var ry = ncyc - ncyu
+            var usermonth = fsplit.slice(1, 2)
+            var nUmonth = Number(usermonth)
+            var nAmonth = Number(mesAtual)
 
-            if (ncmc < ncmu || ncmc == ncmu && ncdc < ncdu ) {
+            var uyear = fsplit.slice(2, 3)
+            var nUyear = Number(uyear)
+            var nAyear = Number(anoAtual)
+            var ry = nAyear - nUyear
+
+            if (nAmonth < nUmonth || nAmonth == nUmonth && nAday < nUday) {
                 ry--;
             }
-
-            if(ry >= 18){
-                mage.push(user)  
+            if (ry >= 18) {
+                mage.push(user)
             }
         });
 
         return response.status(200).json(mage);
     }
 //Ordenar usuários em ordem Afabética \/
-    public async order(request:Request, response:Response){
-        const {organize} = request.headers;
+    public async order(request: Request, response: Response) {
+        const { organize } = request.headers;
         let userAux: iuser[] = []
 
-        if (organize === "desc"){
+        if (organize === "desc") {
             userAux = users.sort((a, b) => {
                 if (a.name.toUpperCase < b.name.toUpperCase) {
-                  return 1;
+                    return 1;
                 }
                 if (a.name > b.name) {
-                  return -1;
+                    return -1;
                 }
-                  return 0;
-              });
-        }else{
+                return 0;
+            });
+        } else {
             userAux = bubbleSort(users)
         }
 
         return response.status(200).json(userAux)
 
     }
+//Valida se o cpf está no banco de dados e registra um Endereço ao usuário \/
+    public async regAddr(request: Request, response: Response) {
 
-}
-function bubbleSort(a:iuser[]){
-            for(var i = 0; i < a.length; i++) {
-                for(var j=0; j < a.length; j++) {
-                    if(a[i].name.toUpperCase < a[j].name.toUpperCase) {
-                        var temp = a[i].name;
-                        a[i].name = a[j].name;
-                        a[j].name = temp;       
-                    }
-                }
+        const { cpf } = request.headers;
+
+        console.log(cpf);
+        if (cpf) {
+            const fuser = users.find((user: any) => {
+                console.log(user)
+                return user.cpf === Number(cpf)
+            });
+            if (!fuser) {
+                return response.status(404).json({ message:"Usuario não existe"});
             }
 
-            return a;
+            const { street, number, district, city, state } = request.body;
+
+            fuser.address = {
+                street,
+                number,
+                district,
+                city,
+                state
+            }
+
+            return response.status(200).json(fuser)
         }
+        return response.status(404).json({ message:"Cpf não preechido"});
+
+    }
+}
+//BubbleSort utilizado em order \/
+function bubbleSort(a: iuser[]) {
+    for (var i = 0; i < a.length; i++) {
+        for (var j = 0; j < a.length; j++) {
+            if (a[i].name.toUpperCase < a[j].name.toUpperCase) {
+                var temp = a[i].name;
+                a[i].name = a[j].name;
+                a[j].name = temp;
+            }
+        }
+    }
+
+    return a;
+}
+
--- src/routes.ts
+++ src/routes.ts
@@ -5,9 +5,10 @@ import UsersController from "./controllers/UsersController";
 const routes = Router();
 const userController = new UsersController();
 
-routes.post("/users", userController.create);
-routes.get("/users", userController.search);
+routes.post("/userCreat", userController.create);
+routes.get("/userFind", userController.search);
 routes.get("/agefind", userController.agefind);
-routes.get ("/order",userController.order)
+routes.get ("/order",userController.order);
+routes.post("/regAddr",userController.regAddr)
 
 export { routes };
